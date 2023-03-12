b13aa3b39e91085201efd34a037cc920d5c702a0
Gatão <gabrielgianotti5@gmail.com>
2023-03-12 18:06:50 -0300

    Update motor.ino

--- motor.ino
+++ motor.ino
@@ -1,23 +1,58 @@
-#include <Servo.h>
-
-Servo ESC;
-
-void setup() {
-  ESC.attach(9,1000,2000);
-  Serial.begin(9600);
-  delay(1000);
-}
-
-void loop() {
-  for(int i = 0; i < 180; i ++){
-   Serial.println(i);
-   ESC.write(i);
-   delay(100);
-  }
-  for(int i = 180; i > 0; i --){
-   Serial.println(i);
-   ESC.write(i);   
-   delay(100);
-  }
-  delay(5000);
+void stabilizeFlight(int speedMotor){
+    mpu6050.update();
+  
+    int x = mpu6050.getAngleX();
+    int y = mpu6050.getAngleY();
+    anguloZ = mpu6050.getAngleZ();
+    int speedRearMotor = speedMotor + 5;
+    Serial.print(x);
+    Serial.print("x ");
+    Serial.print(y);
+    Serial.print("y ");
+    Serial.print(speedMotor);
+    if(x < 0 && y < 0 && activeMotor == HIGH) {
+      Serial.print("rear left ");
+//      Serial.print(5+speedRearMotor);//(-1 * x + 100) * speedMotor / 100);
+      Serial.print(speedMotor);
+      rearLeftEngine.write(5 + speedRearMotor);
+      rearRightEngine.write(speedRearMotor);
+      frontLeftEngine.write(speedMotor);
+      frontRightEngine.write(speedMotor);
+    }
+    else if(x > 0 && y < 0 && activeMotor == HIGH) {
+      Serial.print("rear right ");
+      Serial.print(speedRearMotor);
+      rearLeftEngine.write(speedRearMotor);
+      rearRightEngine.write(5+speedRearMotor);
+      frontLeftEngine.write(speedMotor);
+      frontRightEngine.write(speedMotor);
+    }
+     else if(x < 0 && y > 0 && activeMotor == HIGH) {
+     Serial.print("front left ");
+//      Serial.print((-1 * x + 100) * speedMotor / 100);
+//      Serial.print(" front right ");
+//      Serial.print((x+100) * speedMotor/100);
+//      Serial.print(" speedMotor:");
+      Serial.print(speedMotor);
+      rearLeftEngine.write(speedRearMotor);
+      rearRightEngine.write(speedRearMotor);
+      frontLeftEngine.write(5 + speedMotor);
+      frontRightEngine.write(speedMotor);
+    }
+     else if(x > 0 && y > 0 && activeMotor == HIGH) {
+      Serial.print("front right ");
+//      Serial.print(5 + speedMotor);
+//      Serial.print(" speedMotor:");
+      Serial.print(speedMotor);
+      rearLeftEngine.write(speedRearMotor);
+      rearRightEngine.write(speedRearMotor);
+      frontLeftEngine.write(speedMotor);
+      frontRightEngine.write(5 + speedMotor);
+    }else if(activeMotor == HIGH) {
+      rearLeftEngine.write(speedRearMotor);
+      rearRightEngine.write(speedRearMotor);
+      frontLeftEngine.write(speedMotor);
+      frontRightEngine.write(speedMotor);
+    }
+    Serial.println();  
 }
